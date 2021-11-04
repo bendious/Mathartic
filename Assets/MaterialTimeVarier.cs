bfa570949d31bed8a83dbad65e013147cd2d7650
bendious <benjamin.laws@comcast.net>
2021-11-04 15:21:42 -0500

    Expression-->Material hookup
    - greatly simplified shader/material to make them simply lookup a texture based on screenspace UV
    - MaterialTimeVarier logic to evaluate expressions for each X/Y pixel at the current time T and output/update the material texture
    	- removed OnDestroy() now that material properties don't need to be reset
    
    Todo: performance! rename material/shader?

--- Assets/MaterialTimeVarier.cs
+++ Assets/MaterialTimeVarier.cs
@@ -1,8 +1,8 @@
 using NCalc;
+using System;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Assertions;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 
@@ -20,59 +20,75 @@ public class MaterialTimeVarier : MonoBehaviour
 
 	void Update()
 	{
-		// evaluate given expression strings
+		// get expressions
 		// TODO: move out of per-frame logic?
 		InputField[] fields = { m_rField, m_gField, m_bField };
 		Expression[] expressions = fields.Select(field => ExpressionFromField(field)).ToArray();
 		Text[] errorTexts = { m_rErrorText, m_gErrorText, m_bErrorText };
 
-		for (int i = 0, n = expressions.Length; i < n; ++i)
+		// get texture
+		Material material = GetComponent<Image>().material;
+		Assert.IsNotNull(material);
+		Texture2D texture = (Texture2D)material.GetTexture("_MainTex");
+		if (texture == null)
 		{
-			// skip evaluating while empty or currently being edited
-			Expression exp = expressions[i];
-			if (exp == null || EventSystem.current.currentSelectedGameObject == fields[i].gameObject)
-			{
-				continue;
-			}
+			texture = new Texture2D(Screen.width, Screen.height);
+		}
 
-			// attempt to evaluate
-			Text errorText = errorTexts[i];
-			try
+		// loop over pixels
+		// TODO: optimize!
+		Unity.Collections.NativeArray<Color32> outputColors = texture.GetRawTextureData<Color32>();
+		int outputIdx = 0;
+		bool hitError = false;
+		bool update = false;
+		for (int y = 0, h = texture.height; y < h; ++y)
+		{
+			for (int x = 0, w = texture.width; x < w; ++x, ++outputIdx)
 			{
-				errorText.text = exp.Evaluate().ToString();
-			}
-			catch (System.Exception e)
-			{
-				errorText.text = e.Message;
+				Color32 outputColor = new Color32(0, 0, 0, 255);
+				for (int i = 0, n = expressions.Length; i < n; ++i)
+				{
+					// skip evaluating while empty
+					Expression exp = expressions[i];
+					if (exp == null)
+					{
+						continue;
+					}
+
+					// attempt to evaluate
+					Text errorText = errorTexts[i];
+					try
+					{
+						exp.Parameters["x"] = x / (float)w;
+						exp.Parameters["y"] = y / (float)h;
+						object output = exp.Evaluate();
+						outputColor[i] = (byte)(Convert.ToSingle(output) * byte.MaxValue);
+						errorText.text = "";
+					}
+					catch (Exception e)
+					{
+						errorText.text = e.Message;
+						hitError = true;
+					}
+				}
+				if (hitError)
+				{
+					return;
+				}
+				if (outputColors[outputIdx].r != outputColor.r || outputColors[outputIdx].g != outputColor.g || outputColors[outputIdx].b != outputColor.b)
+				{
+					outputColors[outputIdx] = outputColor;
+					update = true;
+				}
 			}
 		}
 
-		// update material properties
-		Material material = GetComponent<Image>().material;
-		Assert.IsNotNull(material);
-		Shader shader = material.shader;
-		Assert.IsNotNull(shader);
-		for (int i = 0, n = shader.GetPropertyCount(); i < n; ++i)
+		// update GPU
+		// TODO: optimize!
+		if (update)
 		{
-			Assert.IsTrue(shader.GetPropertyType(i) == UnityEngine.Rendering.ShaderPropertyType.Vector);
-			int nameId = shader.GetPropertyNameId(i);
-			Vector4 vecOld = material.GetVector(nameId);
-			Vector4 offsetPcts = (new Vector4(Random.value, Random.value, Random.value, Random.value) * 2.0f - Vector4.one) * m_speedScalar * Time.deltaTime;
-			material.SetVector(nameId, vecOld + offsetPcts);
-		}
-	}
-
-	void OnDestroy()
-	{
-		// reset material since changes to UI Image materials during play persist afterward...
-		Material material = GetComponent<Image>().material;
-		Assert.IsNotNull(material);
-		Shader shader = material.shader;
-		Assert.IsNotNull(shader);
-		for (int i = 0, n = shader.GetPropertyCount(); i < n; ++i)
-		{
-			Assert.IsTrue(shader.GetPropertyType(i) == UnityEngine.Rendering.ShaderPropertyType.Vector);
-			material.SetVector(shader.GetPropertyNameId(i), shader.GetPropertyDefaultVectorValue(i));
+			texture.Apply();
+			material.SetTexture("_MainTex", texture);
 		}
 	}
 
@@ -81,6 +97,12 @@ public class MaterialTimeVarier : MonoBehaviour
 	{
 		Assert.IsNotNull(field);
 		string text = field.text;
-		return string.IsNullOrEmpty(text) ? null : new Expression(text);
+		if (string.IsNullOrEmpty(text))
+		{
+			return null;
+		}
+		Expression exp = new Expression(text);
+		exp.Parameters.Add("t", Time.time * m_speedScalar);
+		return exp;
 	}
 }
